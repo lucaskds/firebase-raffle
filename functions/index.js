33d09ef87916f5c1306914e29c5718499a1f0a76
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2020-03-05 01:49:52 -0300

    :sparkles: - Add get methods
    - Get all pool's participants
    - Get all pools

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,57 @@ const admin = require('firebase-admin')
 admin.initializeApp();
 const pools = admin.database().ref('/pools')
 
-exports.createPool = functions.https.onRequest((req, res) => {
+exports.getParticipants = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'GET') {
+      return res.status(404).json({
+        message: 'Not allowed'
+      })
+    }
 
+    let participants = [];
+
+    return pools.child(req.query.pool).child('participants').on('value', (snapshot) => {
+      snapshot.forEach((participant) => {
+        participants.push({
+          name: participant.val().name,
+          email: participant.val().email
+        });
+      });
+
+      res.status(200).json(participants)
+    }, (error) => {
+      res.status(error.code).json({
+        message: `Something went wrong. ${error.message}`
+      })
+    })
+  })
+})
+
+exports.getPools = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'GET') {
+      return res.status(404).json({
+        message: 'Not allowed'
+      })
+    }
+
+    let poolsList = [];
+
+    return pools.on('value', (snapshot) => {
+      snapshot.forEach((pool) => {
+        poolsList.push({
+          name: pool.key
+        });
+      });
+
+      res.status(200).json(poolsList)
+    }, (error) => {
+      res.status(error.code).json({
+        message: `Something went wrong. ${error.message}`
+      })
+    })
+  })
 })
 
 exports.addParticipant = functions.https.onRequest((req, res) => {
